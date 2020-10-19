586c350a657225d939894198e410e7d5b933ac45
youneselb <youneselb@gmail.com>
2020-10-19 14:42:15 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import jokes from "./jokes";
+
+const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
+document.getElementById("jokes").innerHTML = allJokes.join("");
+
